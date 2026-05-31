b0725c21ff304bf99cc469b00cef405a9ad3a7bf
sanhithag <sanhithag9@gmail.com>
2026-05-31 17:08:36 +0530

    Add 0066_Plus_One.py

---
+++ LeetCode/Easy/0066_Plus_One.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def plusOne(self, digits):
+        for i in reversed(range(len(digits))):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
